f6360d8c3f0666748e75e3be9536d79c67346657
Balint Pekker <balint.pekker@cheppers.com>
2022-09-16 12:58:22 +0200

    Issue #3272732: Fixed tests

--- src/EventSubscriber/EmbedButtonSubscriber.php
+++ src/EventSubscriber/EmbedButtonSubscriber.php
@@ -18,17 +18,19 @@ class EmbedButtonSubscriber implements EventSubscriberInterface {
   /**
    * The CKEditor5 Plugin Manager.
    *
-   * @var \Drupal\ckeditor5\Plugin\CKEditor5PluginManagerInterface
+   * @var \Drupal\ckeditor5\Plugin\CKEditor5PluginManagerInterface|null
    */
   protected $ckeditor5PluginManager;
 
   /**
    * Creates EmbedButtonSubscriber object.
    *
-   * @param \Drupal\ckeditor5\Plugin\CKEditor5PluginManagerInterface $ckeditor5_plugin_manager
-   *   The CKEditor5 Plugin Maanger service.
+   * @param \Drupal\ckeditor5\Plugin\CKEditor5PluginManagerInterface|null $ckeditor5_plugin_manager
+   *   The CKEditor5 Plugin Manager service.
    */
-  public function __construct(CKEditor5PluginManagerInterface $ckeditor5_plugin_manager) {
+  public function __construct(
+    CKEditor5PluginManagerInterface $ckeditor5_plugin_manager = NULL
+  ) {
     $this->ckeditor5PluginManager = $ckeditor5_plugin_manager;
   }
 
@@ -49,7 +51,14 @@ class EmbedButtonSubscriber implements EventSubscriberInterface {
    *
    * @return void
    */
-  public function onEmbedButtonUpdate(EmbedButtonEvent $event) {
+  public function onEmbedButtonUpdate(EmbedButtonEvent $event): void {
+    // If the CKEditor5 module is not installed, the service for CKEditor5
+    // would not exist in the system, and when an Embed Button is saved a
+    // Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException
+    // would be thrown. Hence, we do not try to clear cache for CKEditor5.
+    if (!$this->ckeditor5PluginManager) {
+      return;
+    }
     // Invalidate the CKEditor5 plugin cache, so new toolbar items will appear
     // based on how many Embed Buttons are in the system.
     $this->ckeditor5PluginManager->clearCachedDefinitions();
