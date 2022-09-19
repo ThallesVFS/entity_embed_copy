4d746eb0130402ea13ce08cb3f7fc100a2b924c9
Balint Pekker <balint.pekker@cheppers.com>
2022-09-19 14:15:06 +0200

    Issue #3272732: Use default icon on the text format editor screen if not SVG

--- js/entity_embed.set_dynamic_icons.js
+++ js/entity_embed.set_dynamic_icons.js
@@ -5,7 +5,8 @@
       Object.values(drupalSettings.embedButtons || {}).forEach(function (button) {
         // Iterate through the embed buttons and set the corresponding background image.
         let selector = '.ckeditor5-toolbar-button-' + button.id;
-        $(once('entityEmbedSetDynamicIcons', selector, context)).css('background-image', 'url(' + button.icon + ')');
+        let iconUrl = button.icon.endsWith('svg') ? button.icon : '/' + drupalSettings.modulePath + '/js/ckeditor5_plugins/drupalentity/entity.svg';
+        $(once('entityEmbedSetDynamicIcons', selector, context)).css('background-image', 'url(' + iconUrl + ')');
       });
     },
   }
